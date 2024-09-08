0344c7cd8753638bd1e4221b1a03c368a0a27dbe
pon-et <145276703+pon-et@users.noreply.github.com>
2024-09-08 19:06:41 -0400

    second commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,23 @@
+import { TextField } from "@mui/material";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <div style= {{color: 'blue', fontWeight: 'bold'
+      }}>
+        Northeastern Circle K
+      </div>
+
+      <div>
+        ethan
+      </div>
+
+      <TextField id="outlined-basic" label="text" variant="outlined" />
+
       
+    
+
     </div>
   );
 }
