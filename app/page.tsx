4d659b7ac43fd15b2f3864646d3111410098bd26
pon-et <145276703+pon-et@users.noreply.github.com>
2024-09-27 01:26:57 -0400

    removed embedded git repo in components and add as part of main repo

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,16 @@
 import { TextField } from "@mui/material";
 import Image from "next/image";
+import Styles from '../Styles/Header.module.css';
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <div style= {{color: 'blue', fontWeight: 'bold'
-      }}>
-        Northeastern Circle K
-      </div>
-
-      <div>
-        ethan
-      </div>
-
-      <TextField id="outlined-basic" label="text" variant="outlined" />
-
-      
-    
+    <div className={Styles.header}> Northeastern University Circle K
+      <h1 className={Styles.subtitle}>Live to serve. Love to serve.</h1>
 
     </div>
+
+    
+
+
   );
 }
